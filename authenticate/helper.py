bd82bc3148c45a94ec763e102a913283fb0085af
kyawe1 <kk09254010314@gmail.com>
2021-09-08 21:17:36 +0630

    demo check

---
+++ authenticate/helper.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.decorators import user_passes_test
+
+def checkloggedin(request):
+    if request.user.is_authenticated():
+        return False
+    else :
+        return True
